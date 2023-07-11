89828f22ad690f71a5decfa0d7fc2d853ac05cce
DASOM <n980805@gmail.com>
2023-07-12 02:17:59 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("\\    /\\");
-		System.out.println(" )  ( ')");
-		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
+		StringBuffer sb = new StringBuffer();
+		sb.append("\\    /\\\n");
+		sb.append(" )  ( ')\n");
+		sb.append("(  /  )\n");
+		sb.append(" \\(__)|\n");
+
+		System.out.println(sb);
 	}
 }
