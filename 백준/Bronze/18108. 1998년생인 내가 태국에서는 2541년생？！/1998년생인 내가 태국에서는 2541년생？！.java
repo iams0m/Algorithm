3be8c7e58b9089a89146d847c086aaefcb803385
DASOM <n980805@gmail.com>
2023-07-11 10:29:47 +0900

    Update 1998년생인 내가 태국에서는 2541년생？！.java

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		int year = Integer.parseInt(s);
+		String s = br.readLine(); // 한 줄을 읽고 문자열로 반환
+		int year = Integer.parseInt(s); // 반환된 문자열을 정수로 바꿔줌
 		System.out.println(year - 543);
 
 	}
