c9c69f00a9ecc5e4868788fb91ab2fd0a0df9f8b
DASOM <n980805@gmail.com>
2023-07-15 03:04:39 +0900

    [Bronze V] Title: 영수증, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -7,12 +7,13 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
 		int X = Integer.parseInt(br.readLine());
 		int N = Integer.parseInt(br.readLine());
+        
 		int total = 0;
 
-		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			total += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
