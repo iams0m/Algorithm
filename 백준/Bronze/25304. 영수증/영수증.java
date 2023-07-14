7540b9d678e16ce7e77bd02525253ca09bd617d6
DASOM <n980805@gmail.com>
2023-07-15 03:05:59 +0900

    Update 영수증.java

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        	StringTokenizer st;
 
 		int X = Integer.parseInt(br.readLine());
 		int N = Integer.parseInt(br.readLine());
