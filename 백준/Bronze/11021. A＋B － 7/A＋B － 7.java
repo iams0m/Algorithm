b3322f53465ffbf48e2a70d0723c3528aa86cf18
DASOM <n980805@gmail.com>
2023-07-16 03:34:47 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int a = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		for (int i = 1; i <= a; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			System.out.println(
+					"Case #" + i + ": " + (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+		}
+		br.close();
+	}
+}
