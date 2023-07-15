4bb4e114e82841f42bca73000a223ccfc066f168
DASOM <n980805@gmail.com>
2023-07-16 03:07:22 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 860 ms, Memory: 221980 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
