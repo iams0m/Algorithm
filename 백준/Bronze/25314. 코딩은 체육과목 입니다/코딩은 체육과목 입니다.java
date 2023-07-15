d450d1bd4354fcf1e9e4637d6bd7128aaf9cc031
DASOM <n980805@gmail.com>
2023-07-16 03:03:13 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,21 +1,18 @@
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
 
 		int N = Integer.parseInt(br.readLine());
-		String str = "";
 
-		for (int i = 1; i <= N / 4; i++) {
-			str += "long ";
+		for (int i = 0; i < N / 4; i++) {
+			sb.append("long ");
 		}
-		System.out.println(str + "int");
-
+		sb.append("int");
+		System.out.println(sb.toString());
 	}
 }
