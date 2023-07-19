19a3ffe3a82de50ff2af918951abdd9bb74b47c5
DASOM <n980805@gmail.com>
2023-07-19 18:49:28 +0900

    Update 평균.java

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		// 방법 1
 		int n = Integer.parseInt(st.nextToken());
 		int[] score = new int[n];
 		int sum = 0;
@@ -21,6 +21,24 @@ public class Main {
 				max = score[i];
 			}
 		}
+		System.out.println((double) sum * 100 / max / n);
+		
+		// 방법 2(배열 정렬 사용)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int[] score = new int[n];
+		int sum = 0;
+		int max = 0;
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < score.length; i++) {
+			score[i] = Integer.parseInt(st.nextToken());
+			sum += score[i];
+		}
+		Arrays.sort(score);
+		max = score[n - 1];
 		System.out.println((double) sum * 100 / max / n);
 	}
 }
