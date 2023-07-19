d3673319270af5fbe93f6c91bfae8e7d52b3cf16
DASOM <n980805@gmail.com>
2023-07-20 02:22:55 +0900

    Update 바구니 뒤집기.java

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		// 방법 1(StringBuilder 사용)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
@@ -32,5 +33,35 @@ public class Main {
 			sb.append(array[z]).append(" ");
 		}
 		System.out.println(sb);
+
+		// 방법 2
+		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// int n = Integer.parseInt(st.nextToken());
+		// int m = Integer.parseInt(st.nextToken());
+
+		// int[] array = new int[n];
+
+		// for (int i = 0; i < array.length; i++) {
+		// 	array[i] = i + 1;
+		// }
+		// for (int j = 0; j < m; j++) {
+		// 	st = new StringTokenizer(br.readLine(), " ");
+		// 	int x = Integer.parseInt(st.nextToken()) - 1;
+		// 	int y = Integer.parseInt(st.nextToken()) - 1;
+
+		// 	for (int k = x; k <= y; k++, y--) {
+		// 		int temp = array[k];
+		// 		array[k] = array[y];
+		// 		array[y] = temp;
+		// 	}
+		// }
+
+		// for (int z = 0; z < array.length; z++) {
+		// 	System.out.print(array[z] + " ");
+
+		// 	br.close();
+		// }
 	}
 }
