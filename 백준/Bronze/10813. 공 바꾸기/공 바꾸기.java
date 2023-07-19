4047a078e305fed31062e0d0cf9568014fe044d5
DASOM <n980805@gmail.com>
2023-07-20 02:45:48 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -4,30 +4,33 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int[] arr = new int[N];
+		int temp;
 
 		for (int i = 0; i < N; i++) {
 			arr[i] = i + 1;
 		}
 		for (int j = 0; j < M; j++) {
-			int tmp;
-			st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine());
+
 			int X = Integer.parseInt(st.nextToken());
 			int Y = Integer.parseInt(st.nextToken());
 
-			tmp = arr[X - 1];
+			temp = arr[X - 1];
 			arr[X - 1] = arr[Y - 1];
-			arr[Y - 1] = tmp;
+			arr[Y - 1] = temp;
+		}
+
+		for (int k = 0; k < arr.length; k++) {
+			bw.write(arr[k] + " ");
 		}
 		br.close();
-
-		for (int k : arr) {
-			System.out.print(k + " ");
-		}
+		bw.flush();
+		bw.close();
 	}
 }
