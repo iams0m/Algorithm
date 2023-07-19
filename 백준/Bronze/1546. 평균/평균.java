206ab82fedf3c89d2f96d2a7d8f1059184a06de8
DASOM <n980805@gmail.com>
2023-07-19 18:39:41 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int[] score = new int[n];
+		int sum = 0;
+		int max = 0;
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < score.length; i++) {
+			score[i] = Integer.parseInt(st.nextToken());
+			sum += score[i];
+			if (max < score[i]) {
+				max = score[i];
+			}
+		}
+		System.out.println((double) sum * 100 / max / n);
+	}
+}
