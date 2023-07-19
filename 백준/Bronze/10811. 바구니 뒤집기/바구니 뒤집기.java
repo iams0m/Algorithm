187615b367530f6f8e2bf8b111f46736375c8907
DASOM <n980805@gmail.com>
2023-07-20 02:21:27 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -4,32 +4,33 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
+		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
-		int[] array = new int[n];
+		int[] array = new int[n + 1];
 
-		for (int i = 0; i < array.length; i++) {
-			array[i] = i + 1;
+		for (int i = 1; i <= n; i++) {
+			array[i] = i;
 		}
 		for (int j = 0; j < m; j++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int x = Integer.parseInt(st.nextToken()) - 1;
-			int y = Integer.parseInt(st.nextToken()) - 1;
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 
-			for (int k = x; k <= y; k++, y--) {
+			for (int k = x; k <= (x + y) / 2; k++) {
 				int temp = array[k];
-				array[k] = array[y];
-				array[y] = temp;
+				array[k] = array[x + y - k];
+				array[x + y - k] = temp;
 			}
 		}
 
-		for (int z = 0; z < array.length; z++) {
-			System.out.print(array[z] + " ");
-
-			br.close();
+		for (int z = 1; z <= n; z++) {
+			sb.append(array[z]).append(" ");
 		}
+		System.out.println(sb);
 	}
 }
