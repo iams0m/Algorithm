c9f9f3eef6a2ae9672538f2c57729356fd5d7db3
DASOM <n980805@gmail.com>
2023-07-19 18:24:14 +0900

    Update 나머지.java

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		// 방법 1(HashSet 이용)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		HashSet<Integer> h = new HashSet<Integer>();
 
@@ -12,5 +13,21 @@ public class Main {
 			h.add(Integer.parseInt(br.readLine()) % 42);
 		}
 		System.out.println(h.size());
+
+		boolean[] arr = new boolean[42];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 방법 2(boolean 이용)
+		// for (int i = 0; i < 10; i++) {
+		// 	arr[Integer.parseInt(br.readLine()) % 42] = true;
+		// }
+
+		// int count = 0;
+		// for (boolean value : arr) {
+		// 	if (value == true) { // value 가 true 라면
+		// 		count++;
+		// 	}
+		// }
+		// System.out.println(count);
 	}
 }
