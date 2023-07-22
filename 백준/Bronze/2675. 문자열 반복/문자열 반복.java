19e8749ab27e0037137e55b2ea46122ef8de494d
DASOM <n980805@gmail.com>
2023-07-23 01:50:32 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -7,6 +7,7 @@ public class Main {
 
 		int T = Integer.parseInt(br.readLine());
 
+		// 방법 1
 		for (int i = 0; i < T; i++) {
 			String[] str = br.readLine().split(" ");
 			int R = Integer.parseInt(str[0]);
@@ -20,5 +21,18 @@ public class Main {
 			sb.append("\n");
 		}
 		System.out.print(sb);
+
+		// 방법 2
+		// for (int i = 0; i < T; i++) {
+		// 	String[] str = br.readLine().split(" ");
+		// 	int R = Integer.parseInt(str[0]);
+		// 	String S = str[1];
+
+		// 	for (int j = 0; j < S.length(); j++) {
+		// 		for (int k = 0; k < R; k++) {
+		// 			System.out.print(S.charAt(j));
+		// 		}
+		// 	}
+		// 	System.out.println();
 	}
 }
