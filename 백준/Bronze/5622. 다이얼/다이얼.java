f4331bdede4f6895b60347f6f39b70e5ddab078c
DASOM <n980805@gmail.com>
2023-07-24 11:28:27 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -2,44 +2,32 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
 		int count = 0;
-		for (int i = 0; i < str.length(); i++) {
-			switch (str.charAt(i)) {
-			case 'A' : case 'B' : case 'C' :
-				count += 3;
-				break;
-				
-			case 'D' : case 'E' : case 'F' :
-				count += 4;
-				break;
+		int value;
 
-			case 'G' : case 'H' : case 'I' :
+		while (true) {
+			value = System.in.read();
+			if (value == '\n') {
+				break;
+			}
+			if (value < 68) {
+				count += 3;
+			} else if (value < 71) {
+				count += 4;
+			} else if (value < 74) {
 				count += 5;
-				break;
-			
-			case 'J' : case 'K' : case 'L' :
+			} else if (value < 77) {
 				count += 6;
-				break;	
-				
-			case 'M' : case 'N': case 'O' : 
-				count += 7; 
-				break;
-                
-			case 'P' : case 'Q': case 'R' : case 'S' :
-				count += 8; 
-				break;
-                
-			case 'T' : case 'U': case 'V' : 
-				count += 9; 
-				break;
-                
-			case 'W' : case 'X': case 'Y' : case 'Z' : 
-				count += 10; 
-				break;	
+			} else if (value < 80) {
+				count += 7;
+			} else if (value < 84) {
+				count += 8;
+			} else if (value < 87) {
+				count += 9;
+			} else {
+				count += 10;
 			}
 		}
-		System.out.print(count);
+		System.out.println(count);
 	}
 }
