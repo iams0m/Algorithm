4e45cad8f916a2fab137ba86340276120d4ef124
DASOM <n980805@gmail.com>
2023-07-24 11:14:25 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,20 +1,31 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		int[] A = new int[3];
-		int[] B = new int[3];
+		// 방법 1
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		for (int i = 0; i < 3; i++) {
-			A[i] = System.in.read() - '0';
-		}
-		System.in.read();
-		for (int i = 0; i < 3; i++) {
-			B[i] = System.in.read() - '0';
-		}
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 
-		int tA = A[0] + A[1] * 10 + A[2] * 100;
-		int tB = B[0] + B[1] * 10 + B[2] * 100;
-		System.out.println(Math.max(tA, tB));
+		System.out.println(A > B ? A : B);
+
+		// 방법 2
+		// int[] A = new int[3];
+		// int[] B = new int[3];
+
+		// for (int i = 0; i < 3; i++) {
+		// 	A[i] = System.in.read() - '0';
+		// }
+		// System.in.read();
+		// for (int i = 0; i < 3; i++) {
+		// 	B[i] = System.in.read() - '0';
+		// }
+
+		// int tA = A[0] + A[1] * 10 + A[2] * 100;
+		// int tB = B[0] + B[1] * 10 + B[2] * 100;
+		// System.out.println(Math.max(tA, tB));
 	}
 }
