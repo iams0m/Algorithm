917de10814e6be7bdf94a9f4ddcda27fb3e562f5
DASOM <n980805@gmail.com>
2023-07-27 00:55:23 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -3,26 +3,27 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[][] ch = new char[5][15];
-		int max = 0;
+		char[][] ch = new char[5][15]; // 5줄에 최대 15개의 문자를 저장할 2차원 배열 선언
+		int max = 0; // 입력받은 문자열 중 가장 긴 길이를 저장할 변수 선언
 
+		// 5줄에 각각 문자열을 입력받고 가장 긴 문자열의 길이를 구함
 		for (int i = 0; i < ch.length; i++) {
 			String str = br.readLine();
 			max = Math.max(max, str.length());
 			for (int j = 0; j < str.length(); j++) {
-				ch[i][j] = str.charAt(j);
+				ch[i][j] = str.charAt(j); // 문자열을 문자 단위로 2차원 배열에 저장
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
+		// 세로로 읽어서 문자열을 만들고 출력
 		for (int i = 0; i < max; i++) {
 			for (int j = 0; j < 5; j++) {
-				if (ch[j][i] == '\0') {
+				if (ch[j][i] == '\0') { // 배열의 끝을 확인하여 문자가 없으면 건너뜀
 					continue;
 				}
-				sb.append(ch[j][i]);
+				sb.append(ch[j][i]); // 세로로 읽어서 StringBuilder에 추가
 			}
 		}
-		System.out.println(sb);
+		System.out.println(sb); // 결과 출력
 	}
-}
