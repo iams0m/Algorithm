2c3ba703130d067f7d60edca068654591bc29960
DASOM <n980805@gmail.com>
2023-07-26 14:44:07 +0900

    [Bronze III] Title: 최댓값, Time: 160 ms, Memory: 15952 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int[][] arr = new int[9][9];
+
+		int max = 0;
+		int x = 0;
+		int y = 0;
+
+		for (int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (max < arr[i][j]) {
+					max = arr[i][j];
+					x = i;
+					y = j;
+				}
+			}
+		}
+
+		System.out.println(max);
+		System.out.println((x + 1) + " " + (y + 1));
+	}
+}
