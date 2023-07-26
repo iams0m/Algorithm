e0cdba503ac0158fffa44532f771d0c8ba942d67
DASOM <n980805@gmail.com>
2023-07-26 14:53:14 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 16080 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -2,7 +2,9 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+		// 방법 1(StringTokenizer 사용)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
 		int[][] arr = new int[9][9];
 
@@ -11,9 +13,9 @@ public class Main {
 		int y = 0;
 
 		for (int i = 0; i < 9; i++) {
-			String[] str = br.readLine().split(" ");
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 9; j++) {
-				arr[i][j] = Integer.parseInt(str[j]);
+				arr[i][j] = Integer.parseInt(st.nextToken());
 				if (max < arr[i][j]) {
 					max = arr[i][j];
 					x = i;
@@ -24,5 +26,29 @@ public class Main {
 
 		System.out.println(max);
 		System.out.println((x + 1) + " " + (y + 1));
+		
+		// 방법 2(split 사용)
+		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// int[][] arr = new int[9][9];
+
+		// int max = 0;
+		// int x = 0;
+		// int y = 0;
+
+		// for (int i = 0; i < 9; i++) {
+		// 	String[] str = br.readLine().split(" ");
+		// 	for (int j = 0; j < 9; j++) {
+		// 		arr[i][j] = Integer.parseInt(str[j]);
+		// 		if (max < arr[i][j]) {
+		// 			max = arr[i][j];
+		// 			x = i;
+		// 			y = j;
+		// 		}
+		// 	}
+		// }
+
+		// System.out.println(max);
+		// System.out.println((x + 1) + " " + (y + 1));
 	}
 }
