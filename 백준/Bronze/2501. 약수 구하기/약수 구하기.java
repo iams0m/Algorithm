a0910e3b66cc6cf1420e227b0965913d5998da8a
DASOM <n980805@gmail.com>
2023-07-30 01:01:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -9,13 +9,15 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		int count = 0;
-		int result = 0;
+		int count = 0; // 약수의 순서를 저장할 변수
+		int result = 0; // K번째 약수를 저장할 변수
 
+		// 약수 구하는 반복문
 		for (int i = 1; i <= N; i++) {
 			if (N % i == 0) {
-				count++;
+				count++; // 약수이면, count 증가
 			}
+			// count가 K와 같아지면, 곧 그 약수가 K번째 약수란 뜻이기에 result에 저장 후 반복문 종료
 			if (count == K) {
 				result = i;
 				break;
