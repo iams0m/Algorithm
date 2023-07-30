5f23c31d93311c9c9733867d153e6cb6ab2b2d25
DASOM <n980805@gmail.com>
2023-07-31 00:07:44 +0900

    [Bronze I] Title: 약수들의 합, Time: 156 ms, Memory: 15088 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine()), sum = 0;
+		while (n != -1) {
+			sb.setLength(0);
+			sb.append(n + " = 1");
+			sum = 1;
+			for (int i = 2; i < n; i++) {
+				if (n % i == 0) {
+					sb.append(" + " + i);
+					sum += i;
+				}
+			}
+			if (sum == n) {
+				System.out.println(sb);
+			} else {
+				System.out.println(n + " is NOT perfect.");
+			}
+			n = Integer.parseInt(br.readLine());
+		}
+	}
+}
