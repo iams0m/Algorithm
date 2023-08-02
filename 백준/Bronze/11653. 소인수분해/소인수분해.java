8970bbaaf8b3acd50838677ccd4e60b818e6100b
DASOM <n980805@gmail.com>
2023-08-03 00:51:12 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,20 +3,25 @@ import java.io.*;
 public class Main {
 	// 소인수분해 : 어떤 수를 소수인 인수로 분해하는 것
 	public static void main(String[] args) throws IOException {
+		// 1. BufferedReader + StringBuilder
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
 
+		int N = Integer.parseInt(br.readLine());
+		
 		for (int i = 2; i <= Math.sqrt(N); i++) {
 			while (N % i == 0) {
 				sb.append(i).append('\n');
 				N /= i;
 			}
 		}
+
+		// 마지막으로 남은 N이 1이 아니면, N 자체가 소인수임을 의미
 		if (N != 1) {
 			sb.append(N);
 		}
+		
 		System.out.println(sb);
 	}
 }
