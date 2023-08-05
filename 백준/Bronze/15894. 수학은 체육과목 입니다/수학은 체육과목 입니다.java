2734eefd8f8457e6bb6e6452e76bc69202a5b726
DASOM <n980805@gmail.com>
2023-08-06 00:48:12 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
+		// N의 범위가 int의 범위를 초과하므로 long형을 써서 N을 입력 받음
 		long n = Integer.parseInt(br.readLine());
 		
 		System.out.println(n * 4);
