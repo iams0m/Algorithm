fe1d53ce46ba24cb0ce340f911f5fbbd184a10c9
DASOM <n980805@gmail.com>
2023-08-05 17:20:29 +0900

    [Bronze III] Title: 네 번째 점, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -4,41 +4,36 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
-		int x_axis[] = new int[3];
-		int y_axis[] = new int[3];
+		int[] x = new int[3];
+		int[] y = new int[3];
 
 		for (int i = 0; i < 3; i++) {
 			st = new StringTokenizer(br.readLine());
-			x_axis[i] = Integer.parseInt(st.nextToken());
-			y_axis[i] = Integer.parseInt(st.nextToken());
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
 		}
-		br.close();
 
-		int x = 0;
-		int y = 0;
-
-		if (x_axis[0] == x_axis[1]) {
-			x = x_axis[2];
-		} else if (x_axis[0] == x_axis[2]) {
-			x = x_axis[1];
+		if (x[0] == x[1]) {
+			sb.append(x[2]);
+		} else if (x[0] == x[2]) {
+			sb.append(x[1]);
 		} else {
-			x = x_axis[0];
+			sb.append(x[0]);
 		}
 
-		if (y_axis[0] == y_axis[1]) {
-			y = y_axis[2];
-		} else if (y_axis[0] == y_axis[2]) {
-			y = y_axis[1];
+		sb.append(" ");
+
+		if (y[0] == y[1]) {
+			sb.append(y[2]);
+		} else if (y[0] == y[2]) {
+			sb.append(y[1]);
 		} else {
-			y = y_axis[0];
+			sb.append(y[0]);
 		}
 
-		bw.write(x + " " + y);
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
