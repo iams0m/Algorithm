fc01792c4c49e35b0b40487e802c622b911b5a1d
DASOM <n980805@gmail.com>
2023-08-09 00:23:53 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // 단일 연산의 경우, 연산을 하는 횟수 만큼 시간 복잡도가 고정적으로 나옴
+        // 시간 복잡도 : O(1)
         System.out.println(1);
         System.out.println(0);
     }
