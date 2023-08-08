a4957f9626adfb8e37ff9e67453f9bd34662f3e1
DASOM <n980805@gmail.com>
2023-08-09 00:44:15 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 136 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -2,6 +2,9 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+        // 이중 for문의 경우, n * n 만큼 시간 소요
+        // 시간 복잡도 : O(n^2)
+        // 차수 : 2
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		long n = Long.parseLong(br.readLine());
 		System.out.println((long) (Math.pow(n, 2)));
