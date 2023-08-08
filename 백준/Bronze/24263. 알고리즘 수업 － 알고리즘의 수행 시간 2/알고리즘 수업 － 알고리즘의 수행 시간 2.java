9f066b99c4db23fe7056335dae5e8beffb620fee
DASOM <n980805@gmail.com>
2023-08-09 00:32:55 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -2,6 +2,9 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+        // for문이 한 번 돌면, for문에 적힌 변수만큼 시간 복잡도가 나옴
+        // 시간 복잡도 : O(n)
+        // 차수 : 1
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		System.out.println(n);
