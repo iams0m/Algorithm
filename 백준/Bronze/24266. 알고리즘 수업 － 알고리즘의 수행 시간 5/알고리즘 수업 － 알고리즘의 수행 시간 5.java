23b9740f336d1285fc26e905fa3b67944c246552
DASOM <n980805@gmail.com>
2023-08-12 00:40:28 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -5,6 +5,9 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		long n = Long.parseLong(br.readLine());
 		System.out.println(n * n * n);
+		// System.out.println((long) Math.pow(n, 3));
+		// Math.pow : double형으로 계산하고 리턴
+		// 너무 큰 long 숫자는 double 형으로 표현시, 오차범위가 존재하여 정확한 계산 어려움
 		System.out.println(3);
 	}
 }
