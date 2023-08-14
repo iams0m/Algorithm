70712f38c0315bb21be982b0af5fa16fa92222e8
DASOM <n980805@gmail.com>
2023-08-14 17:23:11 +0900

    [Bronze III] Title: A+B - 6, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -1,18 +1,19 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringBuffer sb = new StringBuffer();
 
 		int T = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < T; i++) {
-			st = new StringTokenizer(br.readLine(), ",");
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			System.out.println(A + B);
+			String str = br.readLine();
+			int A = str.charAt(0) - '0';
+			int B = str.charAt(2) - '0';
+			
+			sb.append(A + B).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
