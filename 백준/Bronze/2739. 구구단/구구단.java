9ec22902c6db39c433fa4b35f726fd26859221cb
DASOM <n980805@gmail.com>
2023-08-17 08:45:18 +0900

    [Bronze V] Title: 구구단, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,16 @@
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+
 		for (int i = 1; i <= 9; i++) {
-			System.out.println(N + " * " + i + " = " + N * i);
+			sb.append(N).append(" * ").append(i).append(" = ").append(N * i).append('\n');
 		}
+
+		System.out.println(sb);
 	}
 }
