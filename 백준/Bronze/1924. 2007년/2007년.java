8948cf2d4b70dc29376c79887ed2edba4276836c
DASOM <n980805@gmail.com>
2023-08-18 01:57:43 +0900

    [Bronze I] Title: 2007년, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -8,43 +8,16 @@ public class Main {
 
 		int x = Integer.parseInt(st.nextToken());
 		int y = Integer.parseInt(st.nextToken());
+
+		int[] month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		String[] dayOfWeek = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
 		int sum = 0;
-
-		for (int i = 0; i <= x - 1; i++) {
-			if (i == 0) {
-				sum = sum + 0;
-			} else if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
-				sum += 31;
-			} else if (i == 2) {
-				sum += 28;
-			} else {
-				sum += 30;
-			}
+		for (int i = 0; i < x - 1; i++) {
+			sum += month[i];
 		}
-
-		sum = sum + y;
-		switch (sum % 7) {
-		case 0:
-			System.out.println("SUN");
-			break;
-		case 1:
-			System.out.println("MON");
-			break;
-		case 2:
-			System.out.println("TUE");
-			break;
-		case 3:
-			System.out.println("WED");
-			break;
-		case 4:
-			System.out.println("THU");
-			break;
-		case 5:
-			System.out.println("FRI");
-			break;
-		case 6:
-			System.out.println("SAT");
-			break;
-		}
+		
+		sum += y;
+		System.out.println(dayOfWeek[sum % 7]);
 	}
 }
