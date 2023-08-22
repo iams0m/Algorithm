839f0e54eedd505d408b6f19cd3ddb695e0afe6d
DASOM <n980805@gmail.com>
2023-08-23 01:38:09 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -3,19 +3,19 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
 
 		for (int i = n; i >= 1; i--) {
-			for (int j = n - i; j >= 1 ; j--) {
+			for (int j = n - i; j >= 1; j--) {
 				sb.append(" ");
 			}
-			for (int j = 2 * i - 1; j >= 1 ; j--) {
+			for (int j = 2 * i - 1; j >= 1; j--) {
 				sb.append("*");
 			}
 			sb.append('\n');
 		}
-		
+
 		for (int i = 2; i <= n; i++) {
 			for (int j = 1; j <= n - i; j++) {
 				sb.append(" ");
@@ -25,6 +25,7 @@ public class Main {
 			}
 			sb.append('\n');
 		}
+        
 		System.out.print(sb);
 	}
 }
