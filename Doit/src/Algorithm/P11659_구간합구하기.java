4b1c2d54d4b8e8df6af14db031df38085100ee8e
DASOM <n980805@gmail.com>
2023-09-01 08:35:31 +0900

    P11659_구간합구하기.java

--- Doit/src/Algorithm/P11659_구간합구하기.java
+++ Doit/src/Algorithm/P11659_구간합구하기.java
@@ -8,20 +8,24 @@ import java.util.StringTokenizer;
 public class P11659_구간합구하기 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(bufferReader.readLine());
-		int suNo = Integer.parseInt(stringTokenizer.nextToken());
-		int quizNo = Integer.parseInt(stringTokenizer.nextToken());
-		long[] S = new long[suNo + 1];
-		stringTokenizer = new StringTokenizer(bufferReader.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int suNo = Integer.parseInt(st.nextToken()); // 숫자 개수
+		int quizNo = Integer.parseInt(st.nextToken()); // 질의 개수
+		long[] S = new long[suNo + 1]; // 합 배열 선언
+		st = new StringTokenizer(br.readLine());
+
+		// 합 배열 생성
 		for (int i = 1; i <= suNo; i++) {
-			S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
 		}
+		// 구간 합 생성
 		for (int q = 0; q < quizNo; q++) {
-			stringTokenizer = new StringTokenizer(bufferReader.readLine());
-			int i = Integer.parseInt(stringTokenizer.nextToken());
-			int j = Integer.parseInt(stringTokenizer.nextToken());
-			System.out.println(S[j] - S[i - 1]);
+			st = new StringTokenizer(br.readLine());
+			// 질의 범위 받기
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			System.out.println(S[j] - S[i - 1]); // 합 배열을 이용하여 구간 합 출력하기
 		}
 
 	}
