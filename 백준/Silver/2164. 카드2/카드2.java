aee8f13bf2271b8c75da6a6307a1a21ad1579575
DASOM <n980805@gmail.com>
2023-09-03 22:37:22 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45500 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,19 +5,17 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Queue<Integer> myQueue = new LinkedList<Integer>();
+		Queue<Integer> myQueue = new LinkedList<>();
 		int N = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			myQueue.add(i);
 		}
 		while (myQueue.size() > 1) {
-			myQueue.poll();
-			int temp = myQueue.poll();
-			myQueue.add(temp);
+			myQueue.poll(); // 맨 앞의 원소 버림
+			myQueue.add(myQueue.poll()); // 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입
 		}
-		System.out.println(myQueue.poll());
+		System.out.println(myQueue.poll()); // 마지막으로 남은 원소 출력
 	}
 }
