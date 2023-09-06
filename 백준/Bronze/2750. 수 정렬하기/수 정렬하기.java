283be065749d0ff58fb306441d08fd7618418cbf
DASOM <n980805@gmail.com>
2023-09-06 09:09:00 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 15296 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -5,24 +5,25 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		int[] A = new int[N];
+		int[] arr = new int[N];
 		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-        // 버블 정렬
+		// Selection Sort
 		for (int i = 0; i < N - 1; i++) {
-			for (int j = 0; j < N - 1 - i; j++) {
-				if (A[j] > A[j + 1]) {
-					int temp = A[j];
-					A[j] = A[j + 1];
-					A[j + 1] = temp;
+			for (int j = i + 1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
 				}
 			}
 		}
-		for (int i = 0; i < N; i++) {
-		    sb.append(A[i]).append('\n');
+        // 결과 출력
+		for (int val : arr) {
+			sb.append(val).append('\n');
 		}
 		System.out.println(sb);
 	}
