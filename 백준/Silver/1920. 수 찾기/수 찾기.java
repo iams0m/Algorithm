aff2616727af5401f37372cf3f7a9ed7842b3986
DASOM <n980805@gmail.com>
2023-09-14 08:52:19 +0900

    [Silver IV] Title: 수 찾기, Time: 588 ms, Memory: 45244 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] A = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(A);
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			boolean find = false;
-			int target = Integer.parseInt(st.nextToken());
-			int start = 0;
-			int end = A.length - 1;
-			while (start <= end) {
-				int mid_index = (start + end) / 2;
-				int mid_value = A[mid_index];
-				if (mid_value > target) {
-					end = mid_index - 1;
-				} else if (mid_value < target) {
-					start = mid_index + 1;
-				} else {
-					find = true;
-					break;
-				}
-			}
-			if (find) {
-				System.out.println(1);
-			} else {
-				System.out.println(0);
-			}
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            boolean find = false;
+            int target = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = A.length - 1;
+            while (start <= end) {
+                int mid_index = (start + end) / 2;
+                int mid_value = A[mid_index];
+                if (mid_value > target) {
+                    end = mid_index - 1;
+                } else if (mid_value < target) {
+                    start = mid_index + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+        }
+        System.out.print(sb.toString());
+    }
 }
