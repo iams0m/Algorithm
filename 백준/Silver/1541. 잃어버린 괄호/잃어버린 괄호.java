ebbd9062a52f5275da51d5154cec9f94e981abab
DASOM <n980805@gmail.com>
2023-09-17 18:44:24 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+	static int answer = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String example = br.readLine();
+		String[] str = example.split("-");
+		for (int i = 0; i < str.length; i++) {
+			int temp = mySum(str[i]);
+			if (i == 0) {
+				answer = answer + temp;
+			} else {
+				answer = answer - temp;
+			}
+		}
+		System.out.println(answer);
+	}
+	private static int mySum(String example) {
+		int sum = 0;
+		String[] temp = example.split("[+]");
+		for (int i = 0; i < temp.length; i++) {
+			sum = sum + Integer.parseInt(temp[i]);
+		}
+		return sum;
+	}
+}
