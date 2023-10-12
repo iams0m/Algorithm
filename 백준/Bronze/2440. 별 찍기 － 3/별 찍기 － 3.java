e59071354124e80e6ac710bdba91bdaeb75f40ef
DASOM <n980805@gmail.com>
2023-10-12 16:03:21 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -3,9 +3,8 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
 		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
 		for (int i = N; i >= 1; i--) {
 			for (int j = 1; j <= i; j++) {
@@ -13,6 +12,6 @@ public class Main {
 			}
 			sb.append('\n');
 		}
-		System.out.print(sb); // StringBuilder 자체에 toString이 Overriding 되어있어 sb.toString()을 하지 않아도 출력 가능
+		System.out.print(sb);
 	}
 }
