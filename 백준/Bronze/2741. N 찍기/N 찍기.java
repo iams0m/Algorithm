3ad9a86918dc7c890f7e26ef82dba450a3cef009
DASOM <n980805@gmail.com>
2023-10-12 15:45:19 +0900

    [Bronze V] Title: N 찍기, Time: 212 ms, Memory: 18132 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -2,7 +2,6 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		// 풀이 1 - StringBuilder를 사용하여 출력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
@@ -12,12 +11,5 @@ public class Main {
 			sb.append(i).append('\n');
 		}
 		System.out.println(sb);
-
-		// 풀이 2 - System.out.println을 사용하여 출력
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			System.out.println(i);
-		}
 	}
 }
