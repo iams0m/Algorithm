d7b7c83c2eb9c74eca855db658e011e67b504c56
DASOM <n980805@gmail.com>
2023-10-16 11:53:11 +0900

    Update 쉬운 계단 수.java

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,5 +1,6 @@
 import java.io.*;
 
+// 방법 1 - Bottom-Up 방식
 public class Main {
 
 	final static long MOD = 1000000000;
@@ -38,5 +39,60 @@ public class Main {
 			result += dp[N][i];
 		}
 		System.out.println(result % MOD);
+	}
+}
+
+// 방법2 - Top-Down 방식
+public class Mainn {
+
+	static Long[][] dp;
+	static int N;
+	final static long MOD = 1000000000;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dp = new Long[N + 1][10];
+
+		// 첫번째 자릿수는 1로 초기화
+		for (int i = 0; i < 10; i++) {
+			dp[1][i] = 1L;
+		}
+
+		long result = 0;
+
+		// 마지막 자릿수인 1~9까지의 경우의 수를 모두 더해준다.
+		for (int i = 1; i <= 9; i++) {
+			result += recur(N, i);
+		}
+		System.out.println(result % MOD);
+
+	}
+
+	// digit : 자릿수, val : 자릿값
+	// 첫째 자릿수는 각 val이 끝이기 때문에 경우의 수는 1밖에 없다.
+	// 즉, dp[1]의 각 자릿값은 1로 초기화 되어 있어야 한다.
+	static long recur(int digit, int val) {
+		// 첫째 자릿수에 도착한다면, 더이상 탐색할 필요 없음
+		if (digit == 1) {
+			return dp[digit][val];
+		}
+
+		// 해당 자릿수의 val값에 대해 탐색하지 않았을 경우(memoization, 재귀호출 중복 방지)
+		if (dp[digit][val] == null) {
+			// val이 0일 경우, 이전 자리는 1만 가능
+			if (val == 0) {
+				dp[digit][val] = recur(digit - 1, 1);
+			}
+			// val이 9일 경우, 이전 자리는 8만 가능
+			else if (val == 9) {
+				dp[digit][val] = recur(digit - 1, 8);
+			}
+			// 그 외의 경우, val - 1과 val + 1 값의 경우의 수를 합한 경우의 수가 됨
+			else {
+				dp[digit][val] = recur(digit - 1, val - 1) + recur(digit - 1, val + 1);
+			}
+		}
+		return dp[digit][val] % MOD;
 	}
 }
