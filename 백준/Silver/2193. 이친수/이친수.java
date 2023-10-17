8d48a49397a79d353f93d26cfc9941f3a2cf9831
DASOM <n980805@gmail.com>
2023-10-17 11:25:04 +0900

    Update 이친수.java

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 방법 1 - Bottom-Up 방식
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -14,5 +14,30 @@ public class Main {
             d[i] = d[i - 1] + d[i - 2];
         }
         System.out.println(d[N]);
+    }
+}
+
+// 방법 2 - Top-Down 방식
+public class Main {
+    static long d[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        d = new long[N + 1];
+        System.out.println(recur(N));
+    }
+
+    static long recur(int N) {
+        if (N == 0) {
+            return 0;
+        }
+        if (N == 1) {
+            return 1;
+        }
+        if (d[N] > 0) {
+            return d[N];
+        }
+        d[N] = recur(N - 1) + recur(N - 2);
+        return d[N];
     }
 }
