7b75dd899c80f8e4ff8b33f975592a641588e8ba
DASOM <n980805@gmail.com>
2023-10-18 11:45:24 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 14968 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -3,36 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    static Integer[] dp;
-    static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        dp = new Integer[n + 1];
-        arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        int[] arr = new int[n + 1];
 
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
-        dp[0] = 0; // dp 배열을 Integer로 선언해주었기 때문에 0으로 초기화하는 과정 필요
+        
         dp[1] = arr[1];
 
         if (n > 1) {
             dp[2] = arr[1] + arr[2];
         }
 
-        System.out.println(recur(n));
-
-    }
-
-    static int recur(int n) {
-        if (dp[n] == null) {
-            dp[n] = Math.max(Math.max(recur(n - 2), recur(n - 3) + arr[n - 1]) + arr[n], recur(n - 1));
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
         }
-        return dp[n];
+        System.out.print(dp[n]);
     }
 }
