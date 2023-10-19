5f5405f0999e4c7bde5d06384b5c6a65b6fab1ef
DASOM <n980805@gmail.com>
2023-10-20 01:01:12 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 156 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -5,47 +5,34 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] seq;
-    static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        seq = new int[N + 1];
-        dp = new Integer[N + 1];
+        int[] seq = new int[N];
+        int[] dp = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[1] = seq[1];
-        recur(N);
-        int max = 0;
-
-        // 최댓값 찾기
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dp[i]);
-        }
-
-        System.out.print(max);
-
-    }
-
-    static int recur(int N) {
-        if (dp[N] == null) {
-            dp[N] = seq[N];
-            // N - 1부터 1까지 중, N보다 작은 값들을 찾으며 재귀 호출
-            for (int i = N - 1; i >= 1; i--) {
-                if (seq[i] < seq[N]) {
-                    dp[N] = Math.max(recur(i) + seq[N], dp[N]);
-                } else {
-                    recur(i);
+        for (int i = 0; i < N; i++) {
+            dp[i] = seq[i];
+            int num = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (seq[i] > seq[j] && seq[j] > num) {
+                    dp[i] = Math.max(seq[i] + dp[j], dp[i]);
                 }
             }
         }
-        return dp[N];
+
+        // 최댓값 찾기
+        int max = dp[0];
+        for (int i = 1; i < N; i++) {
+            max = Math.max(dp[i], max);
+            }
+        System.out.print(max);
     }
 }
