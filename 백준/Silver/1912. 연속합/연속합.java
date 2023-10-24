b49003cf0dc991b47fc71c8b808ae42632f7ae80
DASOM <n980805@gmail.com>
2023-10-24 10:37:57 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 23960 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,17 +4,14 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    static Integer[] dp;
-    static int max;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
- 
-        arr = new int[n];
-        dp = new Integer[n];
-        
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -22,19 +19,12 @@ public class Main {
 
         // 초기화 하기
         dp[0] = arr[0];
-        max = arr[0];
+        int max = arr[0];
 
-        // dp의 마지막 index는 n - 1이므로 n - 1부터 Top-Down 탐색
-        recur(n - 1);
-
-        System.out.print(max);
-    }
-
-    static int recur(int n) {
-        if (dp[n] == null) {
-            dp[n] = Math.max(recur(n - 1) + arr[n], arr[n]);
-            max = Math.max(dp[n], max);
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(dp[i], max);
         }
-        return dp[n];
+        System.out.print(max);
     }
 }
