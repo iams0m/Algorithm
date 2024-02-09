23e4ac784836086953bcf4c9f31e46bb42918b37
DASOM <bbbangsomi@gmail.com>
2024-02-09 23:42:43 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 1;
+
+        if (n == 0) {
+            System.out.println(result);
+        } else {
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            System.out.println(result);
+        }
+    }
+}
