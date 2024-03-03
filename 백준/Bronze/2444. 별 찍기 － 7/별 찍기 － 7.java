f56d5e8257f2e12f39e266e0c9d4d98622f38691
Han Dasom <bbbangsomi@gmail.com>
2024-03-03 16:40:22 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -7,25 +7,27 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N - i; j++) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append('\n');
         }
 
         for (int i = N - 1; i >= 1; i--) {
             for (int j = 1; j <= N - i; j++) {
-                System.out.print(" ");
+                sb.append(" ");
             }
             for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append('\n');
         }
+        System.out.print(sb);
     }
 }
