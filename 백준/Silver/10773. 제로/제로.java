2c84497415319ecca896e0db9e2c2db109f52957
Han Dasom <bbbangsomi@gmail.com>
2024-03-12 23:23:10 +0900

    [Silver IV] Title: 제로, Time: 244 ms, Memory: 23940 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+
+        for (int n : stack) {
+            sum += n;
+        }
+
+        System.out.print(sum);
+    }
+}
